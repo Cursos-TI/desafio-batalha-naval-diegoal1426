ec34fe5e098c0da152e4ffbcd8325ca834870759
Diego Ferrari <diegoal1426@gmail.com>
2025-03-04 21:17:24 -0300

    Desafio nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#define linhas  10
+#define colunas 10
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -37,9 +38,8 @@ int main() {
     // 0 0 1 0 0
 
     //inicializando o tabuleiro
-    int tabuleiro[10][10];
-    int navio1[3] = {3,3,3};
-    int navio2[3] = {3,3,3};
+    int tabuleiro[linhas][colunas];
+    int navio[3] = {3,3,3};
     //preenchendo o tabuleiro com 0 para representar a agua
     for(int i = 0; i < 10; i++){
         for(int j = 0; j<10;j++){
@@ -48,22 +48,33 @@ int main() {
     }
 
     //montando os navios:
-    //variaveis de apoio para o indice do navio
+    //variaveis de apoio para o indice de posição do navio
     unsigned int horizontalX = 0;
     unsigned int horizontalY = 7;
 
     unsigned int verticalX = 1;
-    unsigned int verticalY = 3;
+    unsigned int verticalY = 1;
 
+    unsigned int diagonal1X = 2;
+    unsigned int diagonal1Y = 4;
+
+    unsigned int diagonal2X = 4;
+    unsigned int diagonal2Y = 4;
     //garante que os navios não colidam no inicio do index
-    if(horizontalX == verticalX ){
+    if(horizontalX == verticalX || diagonal1X == diagonal2X){
         printf("Navios colidindo\n");
-    }else if(horizontalX > 7 || horizontalY > 7 || verticalX > 7 || verticalY > 7){
+    }else if(horizontalX > 7 || horizontalY > 7 || verticalX > 7 || verticalY > 7 || diagonal1X > 7 || diagonal1Y > 7 || diagonal2X > 7 || diagonal2Y > 7 ){
         printf("Navios saindo do tabuleiro\n");
     }else{
         for(int i = 0; i<3;i++){
-            tabuleiro[verticalX+i][verticalY] = navio1[i];
-            tabuleiro[horizontalX][horizontalY+i] = navio2[i];
+            //posiciona o primeiro horizontal
+            tabuleiro[verticalX+i][verticalY] = navio[i];
+            //posiciona o segundo navio vertical
+            tabuleiro[horizontalX][horizontalY+i] = navio[i];
+            //posiciona o terceiro navio diagonal
+            tabuleiro[diagonal1X+i][diagonal1Y+i] = navio[i];
+            //posiciona o quarto navio diagonal
+            tabuleiro[diagonal2X+i][diagonal2Y+i] = navio[i];
         }
     }
 
